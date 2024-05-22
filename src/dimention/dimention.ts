0925f53536c34d23881cba13312303dd2b24cb04
CatilonyZhang <zyc2068521267@gmail.com>
2024-05-22 23:48:16 +0800

    Feat(dimention): Add Dimention class and Interface for Dimention object

---
+++ src/dimention/dimention.ts
@@ -0,0 +1,22 @@
+import { Chunk } from "@/chunk/chunk";
+
+type DimentionPos ={
+    id:number;
+};
+
+interface DimentionInterface{
+    pos: DimentionPos;
+    chunks: Chunk[];
+};
+
+class Dimention implements DimentionInterface{
+    pos: DimentionPos;
+    chunks:Chunk[];
+    
+    constructor(pos:DimentionPos){
+        this.pos=pos;
+        this.chunks=[];
+    }
+};
+
+export {Dimention , DimentionInterface,DimentionPos }
