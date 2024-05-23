099a5a1c072b5d34fa63326ebfa40031031ff87d
YubaC <49724342+YubaC@users.noreply.github.com>
2024-05-23 10:30:21 +0800

    Refactor(chunk): Update Chunk constructor to accept a config object

--- src/chunk/chunk.ts
+++ src/chunk/chunk.ts
@@ -6,21 +6,28 @@ type ChunkPos = {
     y: number;
 };
 
+type ChunkConfig = {
+    pos: ChunkPos;
+    blocks?: Block[];
+    entities?: Entity[];
+};
+
 interface ChunkInterface {
     pos: ChunkPos;
     blocks: Block[];
     entities: Entity[];
 }
+
 class Chunk implements ChunkInterface {
     pos: ChunkPos;
     blocks: Block[];
     entities: Entity[];
 
-    constructor(pos: ChunkPos) {
-        this.pos = pos;
-        this.blocks = [];
-        this.entities = [];
+    constructor(config: ChunkConfig) {
+        this.pos = config.pos;
+        this.blocks = config.blocks || [];
+        this.entities = config.entities || [];
     }
 }
 
-export { Chunk, ChunkInterface, ChunkPos };
+export { Chunk, ChunkInterface, ChunkPos, ChunkConfig };
