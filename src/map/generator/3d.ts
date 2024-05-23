7f0cba0eed82156106ffa9b5583fb8b56dcf886f
YubaC <49724342+YubaC@users.noreply.github.com>
2024-05-23 18:23:26 +0800

    Refactor(MapGenerator): Use generateBlockArray utility function for chunk generation

--- src/map/generator/3d.ts
+++ src/map/generator/3d.ts
@@ -1,24 +1,48 @@
 import { Block, BlockPos } from "@/map/block";
 import { Chunk, ChunkPos } from "@/map/chunk";
+import { generateBlockArray } from "@/map/utils";
 
+/**
+ * A 3D map generator.
+ * @class
+ */
 class MapGenerator {
     seed: string;
 
+    /**
+     * @constructor
+     * @param seed - The seed for the map generator.
+     */
     constructor(seed: string) {
         this.seed = seed;
     }
 
+    /**
+     * Generate a block at the given position.
+     * @param pos The absolute position of the block.
+     * @returns The generated block.
+     */
     generateBlock(pos: BlockPos) {
+        // TODO: Implement block generation based on seed.
         return new Block(pos, "stone");
     }
 
+    /**
+     * Generate a chunk at the given chunk position.
+     * @param pos The position of the chunk to be generated.
+     * @returns The generated chunk.
+     */
     generateChunk(pos: ChunkPos) {
-        const blocks = Array.from({ length: Chunk.SIZE }, () =>
-            Array.from({ length: Chunk.SIZE }, () =>
-                Array.from({ length: Chunk.HEIGHT }, () =>
-                    this.generateBlock({ x: 0, y: 0, z: 0 })
-                )
-            )
+        const blocks = generateBlockArray(
+            { x: Chunk.SIZE, y: Chunk.SIZE, z: Chunk.HEIGHT },
+            (relativePos) => {
+                const absolutePos = {
+                    x: Chunk.SIZE * pos.x + relativePos.x,
+                    y: Chunk.SIZE * pos.y + relativePos.y,
+                    z: relativePos.z
+                };
+                return this.generateBlock(absolutePos);
+            }
         );
 
         return new Chunk({ pos, blocks });
