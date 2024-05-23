5366d1e42b46425a4bc8922cb8715c9a4599601c
YubaC <49724342+YubaC@users.noreply.github.com>
2024-05-23 10:37:16 +0800

    Refactor(dimension): Add comments

--- src/dimension/dimension.ts
+++ src/dimension/dimension.ts
@@ -1,19 +1,41 @@
 import { Chunk } from "@/chunk/chunk";
 
+/**
+ * The configuration for a dimension in the world.
+ * @typedef {Object} DimensionConfig
+ * @property {string} id - The ID of the dimension.
+ * @property {Chunk[]} [chunks] - The chunks in the dimension.
+ */
 type DimensionConfig = {
     id: string;
     chunks?: Chunk[];
 };
 
+/**
+ * The interface for a dimension in the world.
+ * @interface DimensionInterface
+ * @property {string} id - The ID of the dimension.
+ * @property {Chunk[]} chunks - The chunks in the dimension.
+ */
 interface DimensionInterface {
     id: string;
     chunks: Chunk[];
 }
 
+/**
+ * A dimension in the world.
+ * @class
+ * @implements {DimensionInterface}
+ * @extends {DimensionConfig}
+ */
 class Dimension implements DimensionInterface {
-    id: string;
-    chunks: Chunk[];
+    id: string; // The ID of the dimension.
+    chunks: Chunk[]; // The chunks in the dimension.
 
+    /**
+     * @constructor
+     * @param config - The configuration for the dimension.
+     */
     constructor(config: DimensionConfig) {
         this.id = config.id;
         this.chunks = config.chunks || [];
