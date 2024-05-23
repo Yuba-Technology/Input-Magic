a3cc01a9a9befb6087339ba0b527238b3029f418
YubaC <49724342+YubaC@users.noreply.github.com>
2024-05-23 10:31:46 +0800

    Refactor(block): Add comments

--- src/block/block.ts
+++ src/block/block.ts
@@ -1,18 +1,41 @@
+/**
+ * The position of a block in the world.
+ * @typedef {Object} BlockPos
+ * @property {number} x - The x-coordinate of the block.
+ * @property {number} y - The y-coordinate of the block.
+ * @property {number} z - The z-coordinate of the block.
+ */
 type BlockPos = {
     x: number;
     y: number;
     z: number;
 };
 
+/**
+ * The interface for a block in the world.
+ * @interface BlockInterface
+ * @property {BlockPos} pos - The position of the block.
+ * @property {string} type - The type of the block.
+ */
 interface BlockInterface {
     pos: BlockPos;
     type: string;
 }
 
+/**
+ * A block in the world.
+ * @class
+ */
 class Block implements BlockInterface {
     pos: BlockPos;
     type: string;
 
+    /**
+     * @constructor
+     * @param {BlockPos} pos - The position of the block.
+     * @param {string} type - The type of the block.
+     * @returns {Block}
+     */
     constructor(pos: BlockPos, type: string) {
         this.pos = pos;
         this.type = type;
