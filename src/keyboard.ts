48b7df7f5a6cc215b1e9d19708314a39cb65f997
YubaC <49724342+YubaC@users.noreply.github.com>
2024-06-07 10:15:32 +0800

    Docs(keyboard.ts): Fix comments

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -1,7 +1,10 @@
 import { eventBus } from "@/event-bus";
 
 type KeyEvent = {
-    pressedKeys: Set<string>; // The set of pressed keys.
+    /**
+     * The set of pressed keys.
+     */
+    pressedKeys: Set<string>;
 };
 
 /**
@@ -15,8 +18,21 @@ type KeyEvent = {
  * @see {@link eventBus} for the event handling system used.
  */
 class KeyboardManager {
-    useLowerCase: boolean = true; // Whether to use lower case for the single character keys.
+    /**
+     * Whether to use lower case for the single character keys.
+     * For example, when this is set to `true`, the key `A` will be converted to `a`.
+     * Default to `true`.
+     */
+    useLowerCase: boolean = true;
+    /**
+     * The singleton instance of the keyboard manager.
+     * @static
+     */
     private static instance: KeyboardManager; // The singleton instance of the keyboard manager.
+    /**
+     * The set of pressed keys.
+     * @private
+     */
     private pressedKeys: Set<string> = new Set(); // The set of pressed keys.
 
     constructor() {
@@ -37,12 +53,16 @@ class KeyboardManager {
 
     /**
      * Check whether the key is pressed.
-     * @param key The key to be checked.
+     * @param key The key string from the key event.
      * @returns Whether the key is pressed.
      */
     private isKeyPressed(key: string): boolean {
         if (key.length > 1 || key === " ") return this.pressedKeys.has(key);
 
+        // Both upper case and lower case are checked for
+        // single character keys a-z and A-Z.
+        // Because when the shift key is pressed, the key event
+        // will return the upper case character.
         const keyUpper = key.toUpperCase();
         const keyLower = key.toLowerCase();
 
@@ -57,10 +77,13 @@ class KeyboardManager {
      */
     private deleteKey(key: string) {
         if (key.length > 1 || key === " ") {
+            // Not single character key
             this.pressedKeys.delete(key);
             return;
         }
 
+        // Delete both upper case and lower case keys.
+        // See the comment in the isKeyPressed method.
         const keyUpper = key.toUpperCase();
         const keyLower = key.toLowerCase();
 
@@ -75,10 +98,12 @@ class KeyboardManager {
      */
     private handleKeyDown(event: KeyboardEvent) {
         event.preventDefault();
+
+        // Prevent duplicate publishing of the same keychange event.
         if (this.isKeyPressed(event.key)) return;
 
         const key =
-            this.useLowerCase && event.key.length === 1 && event.key !== " "
+            this.useLowerCase && event.key.length === 1 && event.key !== " " // Single character key a-z and A-Z
                 ? event.key.toLowerCase()
                 : event.key;
         this.pressedKeys.add(key);
@@ -138,6 +163,7 @@ class KeyboardManager {
     start() {
         document.addEventListener("keydown", this.handleKeyDown);
         document.addEventListener("keyup", this.handleKeyUp);
+        // Clear the pressed keys when the tab or the broswer is hidden
         document.addEventListener(
             "visibilitychange",
             this.handleVisibilityChange
