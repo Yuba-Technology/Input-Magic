6cf59d553efb077979e23aa4ddaf5b743f29983d
YubaC <49724342+YubaC@users.noreply.github.com>
2024-07-15 16:35:35 +0800

    Feat(Blocks.ts): Add interfaces and classes for different types of blocks
    This commit adds interfaces and classes for different types of blocks, including `BlockSharedProperties`, `LiquidBlockSharedProperties`, `SpreadableBlockSharedProperties`, `BlockUniqueProperties`, `LiquidBlockUniqueProperties`, and `SpreadableBlockUniqueProperties`. These interfaces and classes define the properties and behavior of each type of block, such as hardness, flow distance, spread chance, and spread radius. Shared properties refer to attributes that are common among blocks of the same type, meaning these attributes are the same for all blocks of that type. For example, the hardness attribute for all stone blocks is consistent. Unique properties, on the other hand, refer to attributes that are specific to certain blocks, giving these blocks distinct functionalities or appearances compared to others. For example, different chest blocks can store different sets of items.

--- src/data/map/block.ts
+++ src/data/map/block.ts
@@ -7,15 +7,55 @@ type BlockPos = {
     z: number;
 };
 
+interface BlockSharedProperties {
+    /**
+     * The hardness of the block.
+     */
+    hardness: number;
+}
+
+interface LiquidBlockSharedProperties extends BlockSharedProperties {
+    /**
+     * How far the fluid can flow.
+     */
+    flowDistance: number;
+    /**
+     * The position of the source block.
+     */
+    sourcePos: BlockPos;
+}
+
+interface SpreadableBlockSharedProperties extends BlockSharedProperties {
+    /**
+     * The chance of the block spreading.
+     */
+    spreadChance: number;
+    /**
+     * The spread radius of the block, only blocks within this radius can be spread.
+     */
+    spreadRadius: number;
+}
+
+interface BlockUniqueProperties {}
+
+interface LiquidBlockUniqueProperties extends BlockUniqueProperties {
+    /**
+     * The position of the source block.
+     */
+    saucePos: BlockPos;
+}
+
+interface SpreadableBlockUniqueProperties extends BlockUniqueProperties {}
+
 /**
  * A block in the world.
- * @class
  */
 class Block {
     /**
      * The type of the block.
      */
     type: string;
+    uniqueProperties?: BlockUniqueProperties = undefined;
 
     /**
      * @constructor
@@ -27,4 +67,29 @@ class Block {
     }
 }
 
-export { Block, BlockPos };
+/**
+ * An empty block in the world, like air.
+ */
+class EmptyBlock extends Block {}
+
+class LiquidBlock extends Block {
+    uniqueProperties?: LiquidBlockUniqueProperties = undefined;
+}
+
+class SpreadableBlock extends Block {
+    uniqueProperties?: SpreadableBlockUniqueProperties = undefined;
+}
+
+export {
+    BlockPos,
+    Block,
+    EmptyBlock,
+    LiquidBlock,
+    SpreadableBlock,
+    BlockSharedProperties,
+    LiquidBlockSharedProperties,
+    SpreadableBlockSharedProperties,
+    BlockUniqueProperties,
+    LiquidBlockUniqueProperties,
+    SpreadableBlockUniqueProperties
+};
